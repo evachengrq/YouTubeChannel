e02ddc920f2528c5fc92163d1b0f2529959d4e44
Ruoqian Cheng <cheng.ruoqian@myob.com>
2022-03-11 11:58:34 +0800

    feat: backend prod env deploy

--- ops/bin/build.sh
+++ ops/bin/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-compose -f deployment/docker-compose.pipeline.yml build
+docker-compose -f deployment/docker-compose.pipeline.yml build && \
+docker-compose -f deployment/docker-compose.pipeline.prod.yml build
--- ops/bin/pushImage.sh
+++ ops/bin/pushImage.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-compose -f deployment/docker-compose.pipeline.yml push
+docker-compose -f deployment/docker-compose.pipeline.yml push && \
+docker-compose -f deployment/docker-compose.pipeline.prod.yml push
